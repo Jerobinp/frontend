fb711fdbf32a640907d6fad261f7d29ae998cd99
Jerobinp <jerobinjacob148@gmail.com>
2024-06-01 13:50:27 +0530

    frontendtraining

---
+++ code.js
@@ -0,0 +1,16 @@
+let university = [
+    'Karunya',
+    'Anna'
+]
+let bestuni = true
+let bestplacement = true
+
+console.log(university[0] + ' has a good environment ')
+
+if(bestuni) {
+    console.log(' And ' + university[0] + ' is best university.')
+} else if(bestplacement) {
+    console.log(' And ' + university[0] + ' has best Placements.')
+} else {
+    console.log(' And ' + university[0] + ' is not best university.')
+}
